630984bdcbbd82cee67e22af805205cbd3187a4a
rhylkiio <rhylkiio@gmail.com>
2022-10-20 21:35:20 +0000

    Move to correct Dir

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+nes (16 sloc)  216 Bytes
+
+#include <stdio.h>
+
+/**
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
